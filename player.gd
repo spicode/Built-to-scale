bf21cb4764a2e396ff3b6fb2de0cf5b295eef554
spicode <126559883+spicode@users.noreply.github.com>
2024-09-11 21:12:13 +0300

    Grapling hook
    myble gravity false: "grappling hook"

--- player.gd
+++ player.gd
@@ -14,7 +14,7 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= (gravity * delta)*2
-		print(position)
+		
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -25,3 +25,5 @@ func _physics_process(delta: float) -> void:
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	move_and_slide()
+	
+				
